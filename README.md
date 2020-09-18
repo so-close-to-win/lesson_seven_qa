ab53ec3a87838e85f6ae900328f41a9977777f7a
soclosetowin <sdiliara@yandex.ru>
2020-09-18 11:27:24 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# lesson_seven_qa
+ 
---
+++ src/test/java/tests/some_easy_tests.java
@@ -0,0 +1,23 @@
+package tests;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class some_easy_tests {
+
+    @Test
+    @Tag("positive")
+    void easyPositiveTest() {
+        int a = 1;
+        assertEquals(a, 1);
+    }
+
+    @Test
+    @Tag("antipositive")
+    void easyNegativeTest() {
+        int b = 2;
+        assertEquals(b, 1);
+    }
+}
